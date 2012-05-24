3146a1e859692171efd02e6168c2a25aa6ab60ac
Reed Molbak <norseboar@gmail.com>
2012-05-23 22:57:10 -0500

    First commit!

---
+++ src/parser.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <libxml/xmlreader.h>
+
+int parse(char *filename){
+
+xmlTextReaderPtr
+}
+int main(int argc, char **argv){
+  
+  exit(0);
+}
---
+++ src/parser.h
@@ -0,0 +1,11 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+typedef struct sites{
+  int workerManager;
+  int jobsPerNode;
+  char *url;
+  int port;
+} sites;
+
+#endif
